71104583a5283b6d638cbedde4009e1499afdced
이제훈 <2eejehoon@gmail.com>
2024-03-28 20:11:18 +0900

    user, post 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { PostService } from './post/post.service';
+import { PostController } from './post/post.controller';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PostModule, UserModule],
+  controllers: [AppController, UserController, PostController],
+  providers: [AppService, UserService, PostService],
 })
 export class AppModule {}
